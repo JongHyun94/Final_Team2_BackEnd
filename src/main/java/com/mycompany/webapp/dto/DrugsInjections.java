aee43caf4bb1f5442d4162082b562a0b136816cf
LunaMindy <santacandy@naver.com>
2021-07-03 12:08:18 +0900

    [update] treatmentDto, drugsinjectionsDto, druginjectionListDto

--- src/main/java/com/mycompany/webapp/dto/DrugsInjections.java
+++ src/main/java/com/mycompany/webapp/dto/DrugsInjections.java
@@ -1,5 +1,52 @@
-package com.mycompany.webapp.dto;
+ package com.mycompany.webapp.dto;
+
+import java.util.Date;
 
 public class DrugsInjections {
 
+	private int drugInjectionId;
+	private String drugInjectionDrugInjectionListId;
+	private int drugInjectionTreatmentId;
+	private String drugInjectionIdDoctorId;
+	private int drugInjectionPatientId;
+	private Date drugInjectionRegdate;
+	
+	public int getDrugInjectionId() {
+		return drugInjectionId;
+	}
+	public void setDrugInjectionId(int drugInjectionId) {
+		this.drugInjectionId = drugInjectionId;
+	}
+	public String getDrugInjectionDrugInjectionListId() {
+		return drugInjectionDrugInjectionListId;
+	}
+	public void setDrugInjectionDrugInjectionListId(String drugInjectionDrugInjectionListId) {
+		this.drugInjectionDrugInjectionListId = drugInjectionDrugInjectionListId;
+	}
+	public int getDrugInjectionTreatmentId() {
+		return drugInjectionTreatmentId;
+	}
+	public void setDrugInjectionTreatmentId(int drugInjectionTreatmentId) {
+		this.drugInjectionTreatmentId = drugInjectionTreatmentId;
+	}
+	public String getDrugInjectionIdDoctorId() {
+		return drugInjectionIdDoctorId;
+	}
+	public void setDrugInjectionIdDoctorId(String drugInjectionIdDoctorId) {
+		this.drugInjectionIdDoctorId = drugInjectionIdDoctorId;
+	}
+	public int getDrugInjectionPatientId() {
+		return drugInjectionPatientId;
+	}
+	public void setDrugInjectionPatientId(int drugInjectionPatientId) {
+		this.drugInjectionPatientId = drugInjectionPatientId;
+	}
+	public Date getDrugInjectionRegdate() {
+		return drugInjectionRegdate;
+	}
+	public void setDrugInjectionRegdate(Date drugInjectionRegdate) {
+		this.drugInjectionRegdate = drugInjectionRegdate;
+	}
+	
+	
 }
