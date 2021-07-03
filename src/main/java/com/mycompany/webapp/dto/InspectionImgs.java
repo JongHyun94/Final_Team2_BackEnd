1cb95c10a7482f850b365d534115d4cbdc15260d
JongHyun94 <ant9406@naver.com>
2021-07-03 11:01:04 +0900

    [update] dto 추가 병원 유저 이미지

--- src/main/java/com/mycompany/webapp/dto/InspectionImgs.java
+++ src/main/java/com/mycompany/webapp/dto/InspectionImgs.java
@@ -1,5 +1,41 @@
 package com.mycompany.webapp.dto;
 
 public class InspectionImgs {
-
+	private int inspectionImgId;
+	private int inspectionImgInspectionId;
+	private int inspectionImgOname;
+	private int inspectionImgSname;
+	private int inspectionImgType;
+	
+	public int getInspectionImgId() {
+		return inspectionImgId;
+	}
+	public void setInspectionImgId(int inspectionImgId) {
+		this.inspectionImgId = inspectionImgId;
+	}
+	public int getInspectionImgInspectionId() {
+		return inspectionImgInspectionId;
+	}
+	public void setInspectionImgInspectionId(int inspectionImgInspectionId) {
+		this.inspectionImgInspectionId = inspectionImgInspectionId;
+	}
+	public int getInspectionImgOname() {
+		return inspectionImgOname;
+	}
+	public void setInspectionImgOname(int inspectionImgOname) {
+		this.inspectionImgOname = inspectionImgOname;
+	}
+	public int getInspectionImgSname() {
+		return inspectionImgSname;
+	}
+	public void setInspectionImgSname(int inspectionImgSname) {
+		this.inspectionImgSname = inspectionImgSname;
+	}
+	public int getInspectionImgType() {
+		return inspectionImgType;
+	}
+	public void setInspectionImgType(int inspectionImgType) {
+		this.inspectionImgType = inspectionImgType;
+	}
+	
 }
