9232894647b748548fc8271d37eed52e87d75e88
JongHyun94 <ant9406@naver.com>
2021-07-06 14:35:03 +0900

    [update] 유저 dao

--- src/main/java/com/mycompany/webapp/dao/UsersDao.java
+++ src/main/java/com/mycompany/webapp/dao/UsersDao.java
@@ -12,13 +12,10 @@ import com.mycompany.webapp.dto.Users;
 public interface UsersDao {
 
 	public List<Users> selectAllUser();
-<<<<<<< HEAD
-
 	public List<Users> selectAllDoctors();
 	
-=======
 	public List<Users> selectUsers(@Param("keyword") String keyword, @Param("authority") String authority);
 	public void updateUser(Users user);
 	public void insertUser(Users user);	
->>>>>>> branch 'develop' of https://github.com/JongHyun94/Final_Team2_BackEnd
+
 }
