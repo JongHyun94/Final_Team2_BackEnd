db0b4cc6503b0acc2f9cadce30352b1dbc3c98bf
ParkVitna <bbn960903@gmail.com>
2021-07-06 14:10:03 +0900

    [update] Inspections, Treatment

--- src/main/java/com/mycompany/webapp/dao/TreatmentsDao.java
+++ src/main/java/com/mycompany/webapp/dao/TreatmentsDao.java
@@ -1,8 +1,18 @@
 package com.mycompany.webapp.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
+import com.mycompany.webapp.dto.Treatments;
+
 @Mapper
-public class TreatmentsDao {
+public interface TreatmentsDao {
+
+	public List<Treatments> selectTreatments(String treatmentDate);
+
+	public int updateIstateI(int treatmentId);
+	
+	public int updateIstateC(int treatmentId);
 
 }
