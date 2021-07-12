6f4c975d5e4c4b9c1cdc13646d5b5842951fc647
JongHyun94 <ant9406@naver.com>
2021-07-12 19:47:31 +0900

    [update] 데이터 분석 1번

--- src/main/java/com/mycompany/webapp/service/InspectionsService.java
+++ src/main/java/com/mycompany/webapp/service/InspectionsService.java
@@ -123,7 +123,7 @@ public class InspectionsService {
 		List<InspectionImgs> inspectionImgList = inspectionImgsDao.selectInspectionImgs(inspectionId);
 		
 		for(InspectionImgs inspectionImg : inspectionImgList) {
-			inspectionImg.setInspection_img_path("http://localhost:8080/inspection/images/" + inspectionImg.getInspection_img_id());
+			inspectionImg.setInspection_img_path("http://localhost:8000/inspection/images/" + inspectionImg.getInspection_img_id());
 		}
 		
 		return inspectionImgList;
